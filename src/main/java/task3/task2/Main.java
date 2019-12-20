57712c0012f030074642c5d8e37c2ef0e206866b
nikitakyrpenko <nikitakyrpenko@gmail.com>
2019-12-20 12:42:13 +0200

    fix

--- src/main/java/task3/task2/Main.java
+++ src/main/java/task3/task2/Main.java
@@ -1,21 +1,15 @@
 package task3.task2;
 
+import task3.task2.contoller.ShopController;
+
 public class Main {
 
-
-
     public static void main(String... args){
-        Shop shop = new Shop("shopName");
-        Shop.Department department = shop.new Department("test1","loc1","product1","service1");
-        Shop.Department department2 = shop.new Department("test2","loc1","product2","service2");
 
-        shop.pushDepartment(department);
-        shop.pushDepartment(department2);
-        System.out.println(shop);
+        ShopController shopController = new ShopController();
+        shopController.startDialog();
 
-        shop.deleteDepartment(department);
-        System.out.println(shop);
+
+
     }
-
-
 }
