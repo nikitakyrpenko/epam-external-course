5509a737b6a068beb35574dd3f97107fb14d3d2b
nikitakyrpenko <nikitakyrpenko@gmail.com>
2019-12-24 16:51:02 +0200

    Utils and coffee types

---
+++ src/main/java/project/coffeeshop/CoffeeTypes.java
@@ -0,0 +1,9 @@
+package project.coffeeshop;
+
+public enum  CoffeeTypes {
+
+    ARABICA,
+    ROBUSTA,
+    LIBERICA;
+
+}
---
+++ src/main/java/project/coffeeshop/Utils.java
@@ -0,0 +1,17 @@
+package project.coffeeshop;
+
+import java.util.Random;
+
+public class Utils {
+    private static Random random = new Random();
+
+
+    public static <T extends Enum<?>> T getRandomEnumValue(Class<T> clazz){
+        int x = random.nextInt(clazz.getEnumConstants().length);
+        return clazz.getEnumConstants()[x];
+    }
+
+    public static  int pickRandom(int bound){
+        return random.nextInt(bound);
+    }
+}
