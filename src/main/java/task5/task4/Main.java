ecea396962a73b82a1e41738cb906cf4b4d5b814
nikitakyrpenko@gmail.com <nikitakyrpenko@gmail.com>
2020-01-09 20:03:29 +0200

    red black tree

---
+++ src/main/java/task5/task4/Main.java
@@ -0,0 +1,16 @@
+package task5.task4;
+
+public class Main {
+    public static void main(String[] args) {
+        RBTree<Integer, Integer> tree = new RBTree<>();
+        tree.add(3,3);
+        tree.add(2,2);
+        tree.add(1,1);
+        tree.add(7,7);
+
+
+
+
+
+    }
+}
